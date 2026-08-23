79f2b3a8c1c6e8c4df8a3d93be03c3ca7baafe41
hitanshudhakrey07 <hitanshudhakrey07@gmail.com>
2026-08-23 16:14:22 +0530

    Time: 6 ms (3.46%), Space: 46.8 MB (43.78%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(st.isEmpty()) {
+                st.push(nums[i]);
+            }
+            else if(nums[i] == st.peek()) {
+                // duplicate hai, ignore karo
+            }
+            else {
+                st.push(nums[i]);
+            }
+        }
+
+        // Stack ke unique elements ko nums mein copy karo
+        int k = 0;
+        for(int value : st) {
+            nums[k] = value;
+            k++;
+        }
+
+        return st.size();
+    }
+}
